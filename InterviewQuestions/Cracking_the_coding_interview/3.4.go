c46206f88c3195c6ed9777610a927cde3409459b
Rex Chung <rex@rorcraft.com>
2014-06-13 16:27:00 -0700

    recursive tower of hanoi

--- InterviewQuestions/Cracking_the_coding_interview/3.4.go
+++ InterviewQuestions/Cracking_the_coding_interview/3.4.go
@@ -19,11 +19,13 @@ import (
 type Stack struct {
 	cur int
 	mem []int
+	size int
 }
 
 func (s *Stack) push(disk int) {
 	if s.empty() || s.mem[s.cur] > disk {
 		s.cur++
+		s.size++
 		s.mem[s.cur] = disk
 	} else {
 		panic("Cannot push larger disk on to the stack")
@@ -34,11 +36,16 @@ func (s *Stack) empty() bool {
 	return s.cur == -1
 }
 
+func (s *Stack) peek() int {
+	return s.mem[s.cur]
+}
+
 func (s *Stack) pop() int {
 	if s.cur >= 0 {
 		disk := s.mem[s.cur]
 		s.mem[s.cur] = 0
 		s.cur--
+		s.size--
 		return disk
 	} else {
 		panic("Empty stack")
@@ -49,28 +56,36 @@ func NewStack(size int) *Stack {
 	s := new(Stack)
 	s.cur = -1
 	s.mem = make([]int, size)
+	s.size = 0
 	return s
 }
 
-func NewHanoiTowser(size int) (*Stack, *Stack, *Stack) {
-	s1 := NewStack(size)
-	s2 := NewStack(size)
-	s3 := NewStack(size)
-	for i := size; i > 0; i-- {
-		s1.push(i)
-	}
-	return s1, s2, s3
+type HanoiTower struct {
+	s1 *Stack
+	s2 *Stack
+	s3 *Stack
+	N  int
 }
 
-func PrintTower(s1, s2, s3 *Stack) {
-	for i := range s1.mem {
-		fmt.Println(s1.mem[i], s2.mem[i], s3.mem[i])
+func NewHanoiTower(size int) *HanoiTower {
+	ht := new(HanoiTower)
+	ht.s1 = NewStack(size)
+	ht.s2 = NewStack(size)
+	ht.s3 = NewStack(size)
+	for i := size; i > 0; i-- {
+		ht.s1.push(i)
+	}
+	ht.N = size
+	return ht
+}
+
+func (ht *HanoiTower) Print() {
+	for i := 0 ; i < ht.N; i++ {
+		fmt.Println(ht.s1.mem[i], ht.s2.mem[i], ht.s3.mem[i])
 	}
 	fmt.Println("--------------------------------")
 }
 
-func move(from, to) []*Stack {
-}
 // https://en.wikipedia.org/wiki/Tower_of_Hanoi#Solution
 // Number the disks 1(ONE) through n (largest to smallest).
 //
@@ -82,44 +97,79 @@ func move(from, to) []*Stack {
 // No even disk may be placed directly on an even disk.
 // Never undo your previous move (that is, do not move a disk back to its immediate last peg).
 // Considering those constraints after the first move, there is only one legal move at every subsequent turn.
-func MoveHanoiTower(s1, s2, s3 *Stack) {
-	n := len(s1.mem)
+
+// this never finished.
+func (ht *HanoiTower) Solve() {
 	lastMove := make([]*Stack, 2)
 
-	if n % 2 == 1 {
-		s3.push(s1.pop())
+	move := func(from, to *Stack) {
+		to.push(from.pop())
+		lastMove[0] = from
+		lastMove[1] = to
+	}
 
+	validMove := func(from, to *Stack) bool {
+		isUndo := lastMove[0] == to && lastMove[1] == from
+		return !from.empty() &&
+			(to.empty() ||
+			(!to.empty() && (from.peek()%2 != to.peek()%2) && (from.peek() < to.peek()))) &&
+			!isUndo
+	}
+
+	possibleMoves := make([][]*Stack, 6)
+	possibleMoves[0] = []*Stack{ht.s1, ht.s2}
+	possibleMoves[1] = []*Stack{ht.s1, ht.s3}
+	possibleMoves[2] = []*Stack{ht.s2, ht.s3}
+	possibleMoves[3] = []*Stack{ht.s2, ht.s1}
+	possibleMoves[4] = []*Stack{ht.s3, ht.s1}
+	possibleMoves[5] = []*Stack{ht.s3, ht.s2}
+
+	if ht.s1.peek()%2 == 1 {
+		move(ht.s1, ht.s3)
 	} else {
-		s2.push(s1.pop())
+		move(ht.s1, ht.s2)
 	}
-	for !s1.empty() || !s2.empty() {
+	for ht.s3.size <= ht.N {
+		for _, possibleMove := range possibleMoves {
+			if validMove(possibleMove[0], possibleMove[1]) {
+				move(possibleMove[0], possibleMove[1])
+				ht.Print()
+			}
+		}
+	}
+}
 
+// Recursive solution
+// A key to solving this puzzle is to recognize that it can be solved by breaking the problem down into a collection of smaller problems and further breaking those problems down into even smaller problems until a solution is reached. For example:
+//
+// label the pegs A, B, C — these labels may move at different steps
+// let n be the total number of discs
+// number the discs from 1 (smallest, topmost) to n (largest, bottommost)
+// To move n discs from peg A to peg C:
+//
+// move n−1 discs from A to B. This leaves disc n alone on peg A
+// move disc n from A to C
+// move n−1 discs from B to C so they sit on disc n
+func (ht *HanoiTower) SolveRecur() {
+	move := func(from, to *Stack) {
+		to.push(from.pop())
+		ht.Print()
 	}
-	s2.push(s1.pop())
-	PrintTower(s1, s2, s3)
-	s3.push(s1.pop())
-	s3.push(s2.pop())
-	PrintTower(s1, s2, s3)
-	s2.push(s1.pop())
-	PrintTower(s1, s2, s3)
-	s1.push(s3.pop())
-	PrintTower(s1, s2, s3)
-	s2.push(s3.pop())
-	PrintTower(s1, s2, s3)
-	s2.push(s1.pop())
-	PrintTower(s1, s2, s3)
-	s3.push(s1.pop())
-	s3.push(s2.pop())
-	s1.push(s2.pop())
-	s1.push(s3.pop())
-	s3.push(s2.pop())
-	s2.push(s1.pop())
-	s3.push(s1.pop())
-	s3.push(s2.pop())
-	PrintTower(s1, s2, s3)
+	var recur func (n int, src, bri, dst *Stack)
+	recur = func (n int, src, bri, dst *Stack) {
+		if (n == 1) {
+			move(src, dst)
+		} else {
+			recur(n-1, src, dst, bri)
+			move(src, dst)
+			recur(n-1, bri, src, dst)
+		}
+	}
+	recur(ht.N, ht.s1, ht.s2, ht.s3)
 }
 
 func main() {
-	s1, s2, s3 := NewHanoiTowser(6)
-	MoveHanoiTower(s1, s2, s3)
+	ht := NewHanoiTower(6)
+	// ht.Solve()
+	ht.SolveRecur()
 }
